f726c22ef294580968602541471445de8a16cb97
Govorec Leonid <govorecleonid@gmail.com>
2024-07-08 08:57:59 +0300

    asda

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const posts = require('./posts');
+const posts = require('./postsEnglish');
 
 const app = express();
 const PORT = 3000;
