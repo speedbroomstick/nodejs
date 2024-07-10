9f877f4c605d3034a65e6a6ca6528571691e7307
Govorec Leonid <govorecleonid@gmail.com>
2024-07-10 15:41:08 +0300

    new info

--- authorsEnglish.js
+++ authorsEnglish.js
@@ -2,19 +2,23 @@ const authorsEnglish = [
     [
         {
           name: "Floyd Miles",
-          description: "Content Writer @Company"
+          description: "Content Writer @Company",
+          icon: "/avatar/avatar1.png"
         },
         {
           name: "Dianne Russell",
-          description: "Content Writer @Company"
+          description: "Content Writer @Company",
+          icon: "/avatar/avatar2.png"
         },
         {
           name: "Jenny Wilson",
-          description: "Content Writer @Company"
+          description: "Content Writer @Company",
+          icon: "/avatar/avatar3.png"
         },
         {
           name: "Leslie Alexander",
-          description: "Content Writer @Company"
+          description: "Content Writer @Company",
+          icon: "/avatar/avatar4.png"
         }
       ]
 ];
--- authorsRussian.js
+++ authorsRussian.js
@@ -1,19 +1,23 @@
 const authorsRussian = [
         {
           name: "Флойд Майлс",
-          description: "Автор контента @Company"
+          description: "Автор контента @Company",
+          icon: "/avatar/avatar1.png"
         },
         {
           name: "Дианн Рассел",
-          description: "Автор контента @Company"
+          description: "Автор контента @Company",
+          icon: "/avatar/avatar2.png"
         },
         {
           name: "Дженни Вильсон",
-          description: "Автор контента @Company"
+          description: "Автор контента @Company",
+          icon: "/avatar/avatar3.png"
         },
         {
           name: "Лесли Александр",
-          description: "Автор контента @Company"
+          description: "Автор контента @Company",
+          icon: "/avatar/avatar4.png"
         }
 ];
 module.exports = authorsRussian;
