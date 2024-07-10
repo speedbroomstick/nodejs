77db6341817935b03e31e8beca34236ecf0679d4
Govorec Leonid <govorecleonid@gmail.com>
2024-07-10 18:13:02 +0300

    new photo

--- authorsEnglish.js
+++ authorsEnglish.js
@@ -1,43 +1,51 @@
 const authorsEnglish = [
         {
+          id:1,
           name: "Floyd Miles",
           description: "Content Writer @Company",
           icon: "/avatar/avatar1.png"
         },
         {
+          id:2,
           name: "Dianne Russell",
           description: "Content Writer @Company",
           icon: "/avatar/avatar2.png"
         },
         {
+          id:3,
           name: "Jenny Wilson",
           description: "Content Writer @Company",
           icon: "/avatar/avatar3.png"
         },
         {
+          id:4,
           name: "Leslie Alexander",
           description: "Content Writer @Company",
           icon: "/avatar/avatar4.png"
         },
         {
+          id:5,
           name: "Guy Hawkins",
           description: "Content Writer @Company",
           icon: "/avatar/avatar5.png"
         },
         {
+          id:6,
           name: "Eleanor Pena",
           description: "Content Writer @Company",
           icon: "/avatar/avatar6.png"
         },
         {
+          id:7,
           name: "Robert Fox",
           description: "Content Writer @Company",
-          icon: "/avatar/avatar7.png"
+          icon: "/avatar/avatar7.jpg"
         },
         {
+          id:8,
           name: "Jacob Jones",
           description: "Content Writer @Company",
-          icon: "/avatar/avatar8.png"
+          icon: "/avatar/avatar8.jpg"
         }
 ];
 module.exports = authorsEnglish;
--- authorsRussian.js
+++ authorsRussian.js
@@ -1,43 +1,51 @@
 const authorsRussian = [
   {
+      id:1,
       name: "Флойд Майлз",
       description: "Автор контента @Company",
       icon: "/avatar/avatar1.png"
   },
   {
+      id:2,
       name: "Дианна Рассел",
       description: "Автор контента @Company",
       icon: "/avatar/avatar2.png"
   },
   {
+      id:3,
       name: "Дженни Уилсон",
       description: "Автор контента @Company",
       icon: "/avatar/avatar3.png"
   },
   {
+      id:4,
       name: "Лесли Александер",
       description: "Автор контента @Company",
       icon: "/avatar/avatar4.png"
   },
   {
+      id:5,
       name: "Гай Хокинс",
       description: "Автор контента @Company",
       icon: "/avatar/avatar5.png"
   },
   {
+      id:6,
       name: "Элеанор Пена",
       description: "Автор контента @Company",
       icon: "/avatar/avatar6.png"
   },
   {
+      id:7,
       name: "Роберт Фокс",
       description: "Автор контента @Company",
-      icon: "/avatar/avatar7.png"
+      icon: "/avatar/avatar7.jpg"
   },
   {
+      id:8,
       name: "Джейкоб Джонс",
       description: "Автор контента @Company",
-      icon: "/avatar/avatar8.png"
+      icon: "/avatar/avatar8.jpg"
   }
 ];
 module.exports = authorsRussian;
