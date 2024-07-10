4da88a65a748e5dd10cd06b8f06c480a0ffe3cc7
Govorec Leonid <govorecleonid@gmail.com>
2024-07-10 15:32:36 +0300

    new author

--- authorsRussian.js
+++ authorsRussian.js
@@ -1,19 +1,19 @@
 const authorsRussian = [
-    {
-        author: "Алиса Джонсон",
-        description: "10 важных советов и приемов CSS для веб-разработчиков"
-      },
-      {
-        author: "Майкл Браун",
-        description: "Улучшение доступности веб-сайта: лучшие практики и инструменты"
-      },
-      {
-        author: "Эмили Дэвис",
-        description: "Освоение адаптивного дизайна для улучшения пользовательского опыта"
-      },
-      {
-        author: "Дэвид Вилсон",
-        description: "Будущее JavaScript фреймворков: что дальше?"
-      }
+        {
+          name: "Флойд Майлс",
+          description: "Автор контента @Company"
+        },
+        {
+          name: "Дианн Рассел",
+          description: "Автор контента @Company"
+        },
+        {
+          name: "Дженни Вильсон",
+          description: "Автор контента @Company"
+        },
+        {
+          name: "Лесли Александр",
+          description: "Автор контента @Company"
+        }
 ];
 module.exports = authorsRussian;
