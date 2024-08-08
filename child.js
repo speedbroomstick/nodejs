3423ac2f1e8aa82f754b77c59cc9a4f5ed4db5e0
leanidHavarets <leanid.havarets@modsen-software.com>
2024-08-08 15:36:01 +0300

    other important commit

--- child.js
+++ child.js
@@ -2,4 +2,4 @@ process.on('message', (msg) => {
     console.log('Message from parent:', msg);
     process.send({ foo: 'bar' });
   });
-  
+  /////////////////////////////////dfskl;df;lksdf;lksd;lkf;sdk;fksldfk;skdf
